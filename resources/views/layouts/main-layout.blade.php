02323d12be3e570535f2aef81de3004fc6afe219
Mario <lombardomario01@gmail.com>
2022-02-05 12:50:05 +0100

    Sezione "Services" completed

--- resources/views/layouts/main-layout.blade.php
+++ resources/views/layouts/main-layout.blade.php
@@ -5,7 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> --}}
+    <script src="{{ asset('/js/app.js') }}" defer></script>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
 </head>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,8 +5,170 @@
     <section id="services">
         <div class="container">
             <div class="row">
-                <div class="col-7">
-
+                <div class="col-8 ml-6">
+                    <h2 class="title-services mb-2">
+                        Confronto tariffe mobile, internet casa, luce, gas, assicurazioni, conti, carte, pay TV, prestiti e mutui
+                    </h2>   
+                </div>
+                <div class="container-card d-flex flex-column col-7">
+                    <div class="container-number-services mx-auto">
+                        <div>
+                            <span>
+                                <img src="{{ asset('storage/asset/svgexport-10.svg') }}" alt="choose the service">
+                            </span>
+                            <h2 class="first-service">Scegli il servizio</h2>
+                        </div>
+                        <div>
+                            <span>
+                                <img src="{{ asset('storage/asset/svgexport-11.svg') }}" alt="compare the offers">
+                            </span>
+                            <h2>Confronta le offerte</h2>
+                        </div>
+                        <div>
+                            <span>
+                                <img src="{{ asset('storage/asset/svgexport-12.svg') }}" alt="save now">
+                            </span>
+                            <h2>Risparmia ora</h2>
+                        </div>
+                    </div>
+                    <div class="container-card-services mx-auto">
+                        <div>
+                            <a href="#">
+                                <div class="d-flex">
+                                    <span class="d-flex flex-column align-items-center justify-content-center mx-auto">
+                                        <div>
+                                            <img src="{{ asset('storage/asset/svgexport-13.svg') }}" alt="icon1 card">
+                                        </div>
+                                        <span>
+                                            <img class="mini-card" src="{{ asset('storage/asset/svgexport-14.svg') }}" alt="internet casa">
+                                        </span>
+                                        <div class="text-card">
+                                            INTERNET CASA
+                                        </div>
+                                    </span>
+                                </div>
+                            </a>    
+                        </div>
+                        <div>
+                            <a href="#">
+                                <div class="d-flex">
+                                    <span class="d-flex flex-column align-items-center justify-content-center mx-auto">
+                                        <div>
+                                            <img src="{{ asset('storage/asset/svgexport-13.svg') }}" alt="icon2 card">
+                                        </div>
+                                        <span>
+                                            <img class="mini-card" src="{{ asset('storage/asset/svgexport-15.svg') }}" alt="luce">
+                                        </span>
+                                        <div class="text-card">
+                                            LUCE
+                                        </div>
+                                    </span>
+                                </div>        
+                            </a>    
+                        </div>
+                        <div>
+                            <a href="#">
+                                <div class="d-flex">
+                                    <span class="d-flex flex-column align-items-center justify-content-center mx-auto">
+                                        <div>
+                                            <img src="{{ asset('storage/asset/svgexport-13.svg') }}" alt="icon3 card">
+                                        </div>
+                                        <span>
+                                            <img class="mini-card" src="{{ asset('storage/asset/svgexport-16.svg') }}" alt="gas">
+                                        </span>
+                                        <div class="text-card">
+                                            GAS
+                                        </div>
+                                    </span>
+                                </div>
+                            </a>    
+                        </div>
+                        <div>
+                            <a href="#">
+                                <div class="d-flex">
+                                    <span class="d-flex flex-column align-items-center justify-content-center mx-auto">
+                                        <div>
+                                            <img src="{{ asset('storage/asset/svgexport-13.svg') }}" alt="icon4 card">
+                                        </div>
+                                        <span>
+                                            <img class="mini-card" src="{{ asset('storage/asset/svgexport-17.svg') }}" alt="mobile">
+                                        </span>
+                                        <div class="text-card">
+                                            MOBILE
+                                        </div>
+                                    </span>
+                                </div>
+                            </a>    
+                        </div>
+                        <div>
+                            <a href="#">
+                                <div class="d-flex">
+                                    <span class="d-flex flex-column align-items-center justify-content-center mx-auto">
+                                        <div>
+                                            <img src="{{ asset('storage/asset/svgexport-13.svg') }}" alt="icon5 card">
+                                        </div>
+                                        <span>
+                                            <img class="mini-card" src="{{ asset('storage/asset/svgexport-18.svg') }}" alt="conti e carte">
+                                        </span>
+                                        <div class="text-card">
+                                            CONTI E CARTE
+                                        </div>
+                                    </span>
+                                </div>
+                            </a>    
+                        </div>
+                        <div>
+                            <a href="#">
+                                <div class="d-flex">
+                                    <span class="d-flex flex-column align-items-center justify-content-center mx-auto">
+                                        <div>
+                                            <img src="{{ asset('storage/asset/svgexport-13.svg') }}" alt="icon6 card">
+                                        </div>
+                                        <span>
+                                            <img class="mini-card" src="{{ asset('storage/asset/svgexport-19.svg') }}" alt="prestiti e mutui">
+                                        </span>
+                                        <div class="text-card">
+                                            PRESTITI E MUTUI
+                                        </div>
+                                    </span>
+                                </div>
+                            </a>    
+                        </div>
+                        <div>
+                            <a href="#">
+                                <div class="d-flex">
+                                    <span class="d-flex flex-column align-items-center justify-content-center mx-auto">
+                                        <div>
+                                            <img src="{{ asset('storage/asset/svgexport-13.svg') }}" alt="icon7 card">
+                                        </div>
+                                        <span>
+                                            <img class="mini-card" src="{{ asset('storage/asset/svgexport-20.svg') }}" alt="tv">
+                                        </span>
+                                        <div class="text-card">
+                                            TV
+                                        </div>
+                                    </span>
+                                </div>
+                            </a>    
+                        </div>
+                        <div>
+                            <a href="#">
+                                <div class="d-flex">
+                                    <span class="d-flex flex-column align-items-center justify-content-center mx-auto">
+                                        <div>
+                                            <img src="{{ asset('storage/asset/svgexport-13.svg') }}" alt="icon8 card">
+                                        </div>
+                                        <span>
+                                            <img class="mini-card" src="{{ asset('storage/asset/svgexport-21.svg') }}" alt="assicurazioni">
+                                        </span>
+                                        <div class="text-card">
+                                            ASSICURAZIONI
+                                        </div>
+                                    </span>
+                                </div>
+                            </a>    
+                        </div>
+                    </div>
                 </div>
                 <div class="col-5">
                     <img src="{{ asset('storage/asset/header-img.png') }}" class="img-fluid" alt="boat">
